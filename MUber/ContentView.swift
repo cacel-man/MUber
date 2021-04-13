57e8aacaa097e927c310ecf137efdcc12c01feeb
cacel-man <cacel.xxx@gmail.com>
2021-04-13 09:22:33 +0900

    Initial Commit

---
+++ MUber/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MUber
+//
+//  Created by 田中翔 on 2021/04/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MUber/MUberApp.swift
@@ -0,0 +1,17 @@
+//
+//  MUberApp.swift
+//  MUber
+//
+//  Created by 田中翔 on 2021/04/13.
+//
+
+import SwiftUI
+
+@main
+struct MUberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
